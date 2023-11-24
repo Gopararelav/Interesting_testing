9647c53f368e0de700bb3fd1fd0d3b61f2c316dc
Valera <valerakaza11@gmail.com>
2023-11-24 20:48:27 +0500

    sml

--- cfop.py
+++ cfop.py
@@ -1,10 +1,14 @@
 import cv2 as cv
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import numpy
-sc = ImageGrab.grab()
-sc = numpy.array(sc)
+from time import sleep
+# sleep(2)
+# rl = ImageGrab.grab((836, 275+85, 1173, 541+125))
+# d = list(rl.getdata())
+d = [((255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 0)), ((0, 255, 0), (0, 0, 255), (255, 0, 0), (255, 0, 0)), ((0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 0, 0))]
+sc = numpy.array(lst, dtype=numpy.uint8)
 sc = cv.cvtColor(sc, cv.COLOR_BGR2RGB)
-sc = cv.resize(sc, (1300, 700))
+sc = cv.resize(sc, (400, 400))
 cv.imshow("new", sc)
 k = cv.waitKey(0)
 cv.destroyAllWindows()
